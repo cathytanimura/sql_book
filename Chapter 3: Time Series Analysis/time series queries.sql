6ad9e8295251b01a569c2835076d9623263d9ed3
cathytanimura <69559890+cathytanimura@users.noreply.github.com>
2021-05-27 15:55:08 -0700

    Update time series queries.sql

--- Chapter 3: Time Series Analysis/time series queries.sql
+++ Chapter 3: Time Series Analysis/time series queries.sql
@@ -1,1 +1,100 @@
+-- Trending the data
+-- Simple trends
+
+SELECT sales_month
+,sales
+FROM retail_sales
+WHERE kind_of_business = 'Retail and food services sales, total'
+ORDER BY 1
+;
+
+SELECT date_part('year',sales_month) as sales_year
+,sum(sales) as sales
+FROM retail_sales
+WHERE kind_of_business = 'Retail and food services sales, total'
+GROUP BY 1
+ORDER BY 1
+;
+
+-- Comparing components
+SELECT date_part('year',sales_month) as sales_year
+,kind_of_business
+,sum(sales) as sales
+FROM retail_sales
+WHERE kind_of_business in ('Book stores'
+ ,'Sporting goods stores','Hobby, toy, and game stores')
+GROUP BY 1,2
+ORDER BY 1,2
+;
+
+SELECT sales_month
+,kind_of_business
+,sales
+FROM retail_sales
+WHERE kind_of_business in ('Men''s clothing stores'
+ ,'Women''s clothing stores')
+ORDER BY 1,2
+;
+
+SELECT date_part('year',sales_month) as sales_year
+,kind_of_business
+,sum(sales) as sales
+FROM retail_sales
+WHERE kind_of_business in ('Men''s clothing stores'
+ ,'Women''s clothing stores')
+GROUP BY 1,2
+;
+
+SELECT date_part('year',sales_month) as sales_year
+,sum(case when kind_of_business = 'Women''s clothing stores' 
+          then sales 
+          end) as womens_sales
+,sum(case when kind_of_business = 'Men''s clothing stores' 
+          then sales 
+          end) as mens_sales
+FROM retail_sales
+WHERE kind_of_business in ('Men''s clothing stores'
+ ,'Women''s clothing stores')
+GROUP BY 1
+ORDER BY 1
+;
+
+SELECT sales_year
+,womens_sales - mens_sales as womens_minus_mens
+,mens_sales - womens_sales as mens_minus_womens
+FROM
+(
+        SELECT date_part('year',sales_month) as sales_year
+        ,sum(case when kind_of_business = 'Women''s clothing stores' 
+                  then sales 
+                  end) as womens_sales
+        ,sum(case when kind_of_business = 'Men''s clothing stores' 
+                  then sales 
+                  end) as mens_sales
+        FROM retail_sales
+        WHERE kind_of_business in ('Men''s clothing stores'
+         ,'Women''s clothing stores')
+        and sales_month <= '2019-12-01'
+        GROUP BY 1
+) a
+ORDER BY 1
+;
+
+SELECT date_part('year',sales_month) as sales_year
+,sum(case when kind_of_business = 'Women''s clothing stores' 
+          then sales end) 
+ - 
+ sum(case when kind_of_business = 'Men''s clothing stores' 
+          then sales end)
+ as womens_minus_mens
+FROM retail_sales
+WHERE kind_of_business in ('Men''s clothing stores'
+ ,'Women''s clothing stores')
+and sales_month <= '2019-12-01'
+GROUP BY 1
+ORDER BY 1
+;
+
+
+
 
