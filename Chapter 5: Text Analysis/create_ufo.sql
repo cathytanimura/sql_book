91737b03eef19f3f28730fcdb39ad3c07d02c22b
cathytanimura <69559890+cathytanimura@users.noreply.github.com>
2021-05-29 12:02:43 -0700

    Create create_ufo.sql

---
+++ Chapter 5: Text Analysis/create_ufo.sql
@@ -0,0 +1,21 @@
+DROP table if exists ufo;
+CREATE table ufo
+(
+,sighting_report varchar(1000)
+,description text
+)
+;
+
+-- change localpath to the directory where you saved the ufo .csv files
+
+COPY ufo FROM '/localpath/ufo1.csv' DELIMITER ',' CSV HEADER;
+
+COPY ufo FROM '/localpath/ufo2.csv' DELIMITER ',' CSV HEADER;
+
+COPY ufo FROM '/localpath/ufo3.csv' DELIMITER ',' CSV HEADER;
+
+COPY ufo FROM '/localpath/ufo4.csv' DELIMITER ',' CSV HEADER;
+
+COPY ufo FROM '/localpath/ufo5.csv' DELIMITER ',' CSV HEADER;
+
+
